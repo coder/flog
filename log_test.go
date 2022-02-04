acff97948c8e79167b15d1e50891892c42c90742
Ammar Bandukwala <ammar@ammar.io>
2022-02-04 17:05:10 -0600

    Fix tests

--- log_test.go
+++ log_test.go
@@ -7,14 +7,6 @@ import (
 )
 
 func TestLogger(t *testing.T) {
-	log(infoLevel, "hello %.3f", 1/3.0)
-	log(successLevel, "finished that")
-	log(error, "oops")
-
-	log := New().WithPrefix("user %v: ", 500)
-
-	log.log(error, "didn't work out")
-
 	// Short-hand
 
 	Info("something happened")
